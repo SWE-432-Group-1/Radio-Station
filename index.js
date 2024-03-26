23e2448373762e7d0780451403f0d97927e59add
Neer Patel <neerpatel@Neers-MBP.eduroam.gmu.edu>
2024-03-26 14:47:28 -0400

    Added public folder with our static files. Set app's "static/" to our public folder.

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import managerRouter from "./scripts/manager-router.js";
 import producerRouter from "./scripts/producer-router.js";
-import djRouter from "./scripts/dj-router.js";
+import djRouter from "./scripts/dj-router.js"; 
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
+
+// Serve static files from the public directory. 
+app.use('/static', express.static(join(__dirname, "public")));
 
 managerRouter.handleAll(app);
 producerRouter.handleAll(app);
--- scripts/manager-router.js
+++ scripts/manager-router.js
@@ -1,6 +1,12 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const handleAll = (app) => {
     app.get("/manager", (_req, res) => {
-        res.sendFile("views/Manager/manager.html");
+        res.sendFile(join(__dirname, "../public/Manager/manager.html"));
     });
 };
 
