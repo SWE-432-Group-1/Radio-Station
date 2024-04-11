ba2f370e007d292f714da6c1ff883b90bb08ab79
Samuel Rembisz <sjrembisz07@gmail.com>
2024-04-11 18:16:40 -0400

    feat: added mongo : )

--- index.js
+++ index.js
@@ -4,25 +4,36 @@ import producerRouter from "./scripts/producer-router.js";
 import djRouter from "./scripts/dj-router.js";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 
+const mongooseClientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
+await mongoose.connect(process.env.MONGO_URL, mongooseClientOptions);
+await mongoose.connection.db.admin().command({ ping: 1 });
+console.log("Connected to MongoDB");
+
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-// Serve static files from the public directory. 
-app.use('/static', express.static(join(__dirname, "public")));
+// Serve static files from the public directory.
+app.use("/static", express.static(join(__dirname, "public")));
 
-// Used to get data from ejs. 
-app.use(express.urlencoded({extended: true}));
+// Used to get data from ejs.
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Handle all of the routing. 
+// Handle all of the routing.
 managerRouter.handleAll(app);
 producerRouter.handleAll(app);
 djRouter.handleAll(app);
@@ -33,33 +44,74 @@ app.get("/", (_req, res) => {
 
 // Ejs for Producer
 app.get("/Prod", (_req, res) => {
-  const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun',
-    '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14',
-    '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26',
-    '27', '28', '29']
+  const days = [
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thu",
+    "Fri",
+    "Sat",
+    "Sun",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29",
+  ];
 
   const podcasts = [
-    { name: 'Good Podcast', episode: '9', duration: '1hr 30min' },
-    { name: 'Bad Podcast', episode: '10', duration: '1hr' },
-    { name: 'Okay Podcast', episode: '1', duration: '1hr 45min' },
-    { name: 'Awesome Podcast', episode: '12', duration: '1hr 50min' },
-    { name: 'Cool Podcast', episode: '5', duration: '1hr 30min' },
-    { name: 'Cringe Podcast', episode: '7', duration: '1hr 35min' },
-    { name: 'Boring Podcast', episode: '3', duration: '2hr' },
-  ]
+    { name: "Good Podcast", episode: "9", duration: "1hr 30min" },
+    { name: "Bad Podcast", episode: "10", duration: "1hr" },
+    { name: "Okay Podcast", episode: "1", duration: "1hr 45min" },
+    { name: "Awesome Podcast", episode: "12", duration: "1hr 50min" },
+    { name: "Cool Podcast", episode: "5", duration: "1hr 30min" },
+    { name: "Cringe Podcast", episode: "7", duration: "1hr 35min" },
+    { name: "Boring Podcast", episode: "3", duration: "2hr" },
+  ];
 
   const songs = [
-    { name: 'Vibin', artist: 'Joe Smith' },
-    { name: 'Back to back', artist: 'Jim Jones' },
-    { name: 'Ghosted', artist: 'Polly Grace' },
-    { name: 'Golden Dust', artist: 'Shiny' },
-    { name: 'Whispers', artist: 'Paranoia' },
-    { name: 'Our Galaxy', artist: 'Cosmos' },
-    { name: 'Dreamy', artist: 'Andy A.' },
-    { name: 'Vast Landscape', artist: 'Hustler' },
-    { name: 'Night thoughts', artist: 'Y' },
-  ]
-  res.render(join(__dirname, "views/partials/Producer/pages/index"), { days: days, podcasts: podcasts, songs: songs, podHandler: "podSearch();", djHandler: "djSearch();", userHandler: "user();" });
+    { name: "Vibin", artist: "Joe Smith" },
+    { name: "Back to back", artist: "Jim Jones" },
+    { name: "Ghosted", artist: "Polly Grace" },
+    { name: "Golden Dust", artist: "Shiny" },
+    { name: "Whispers", artist: "Paranoia" },
+    { name: "Our Galaxy", artist: "Cosmos" },
+    { name: "Dreamy", artist: "Andy A." },
+    { name: "Vast Landscape", artist: "Hustler" },
+    { name: "Night thoughts", artist: "Y" },
+  ];
+  res.render(join(__dirname, "views/partials/Producer/pages/index"), {
+    days: days,
+    podcasts: podcasts,
+    songs: songs,
+    podHandler: "podSearch();",
+    djHandler: "djSearch();",
+    userHandler: "user();",
+  });
 });
 
 const LISTEN_PORT = process.env.RADIO_STATION_LISTEN_PORT || 8080;
---
+++ models/schemas.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+export const TimeslotSchema = new Schema({
+  start: {
+    type: Date,
+    required: true,
+  },
+  end: {
+    type: Date,
+    required: true,
+  },
+});
+export const Timeslot = mongoose.model("Timeslot", TimeslotSchema);
+
+export const DjSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+});
+export const Dj = mongoose.model("Dj", DjSchema);
+
+export const SongSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  artist: {
+    type: String,
+    required: true,
+  },
+});
+export const Song = mongoose.model("Song", SongSchema);
+
+export const PlaylistSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  dj: {
+    type: ObjectId,
+    ref: "Dj",
+    required: true,
+  },
+  songs: {
+    type: [
+      {
+        song: {
+          type: ObjectId,
+          ref: "Song",
+          required: true,
+        },
+        producer_created: {
+          type: Boolean,
+          required: true,
+        },
+      },
+    ],
+    required: true,
+  },
+  timeslot: {
+    type: ObjectId,
+    ref: "Timeslot",
+    required: true,
+  },
+});
+export const Playlist = mongoose.model("Playlist", PlaylistSchema);
