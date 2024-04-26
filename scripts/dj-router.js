b495c0b589e4870668844bb18704f5764ab08816
Samuel Rembisz <sjrembisz07@gmail.com>
2024-04-25 20:12:32 -0400

    feat: implemented most db calls, need to fix ordering and song adding slightly

--- scripts/dj-router.js
+++ scripts/dj-router.js
@@ -1,230 +1,316 @@
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import { Playlist, Song, Timeslot } from "../models/schemas.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const playlistRows = [
-  { id: 1, position: 1, title: "Song 1", producer: "Producer 1" },
-  { id: 2, position: 2, title: "Song 2", producer: "Producer 2" },
-  { id: 3, position: 3, title: "Song 3", producer: "Producer 3" },
-  { id: 4, position: 4, title: "Song 4", producer: "Producer 4" },
-  { id: 5, position: 5, title: "Song 5", producer: "Producer 5" },
-];
-
-const timeslotList = [
-  {
-    id: 1,
-    date: "1969-01-01",
-    start_time: "10:00",
-    end_time: "11:00",
-    playlist: playlistRows.map((row) => ({
-      ...row,
-      title: `${row.title} (10am)`,
-    })),
-  },
-  {
-    id: 2,
-    date: "1969-01-01",
-    start_time: "11:00",
-    end_time: "12:00",
-    playlist: playlistRows.map((row) => ({
-      ...row,
-      title: `${row.title} (11am)`,
-    })),
-  },
-  {
-    id: 3,
-    date: "1969-01-01",
-    start_time: "12:00",
-    end_time: "13:00",
-    playlist: playlistRows.map((row) => ({
-      ...row,
-      title: `${row.title} (12pm)`,
-    })),
-  },
-  {
-    id: 4,
-    date: "1969-01-01",
-    start_time: "13:00",
-    end_time: "14:00",
-    playlist: playlistRows.map((row) => ({
-      ...row,
-      title: `${row.title} (1pm)`,
-    })),
-  },
-  {
-    id: 5,
-    date: "1969-01-01",
-    start_time: "14:00",
-    end_time: "15:00",
-    playlist: playlistRows.map((row) => ({
-      ...row,
-      title: `${row.title} (2pm)`,
-    })),
-  },
-];
-
 const handleAll = (app) => {
-  app.get("/dj", (req, res) => {
-    if (timeslotList.length === 0) {
+  app.get("/dj", async (_req, res) => {
+    let timeslot;
+    try {
+      timeslot = await Timeslot.findOne();
+      if (timeslot === null || timeslot === undefined) {
+        throw new Error("Timeslot query returned null");
+      }
+    } catch (error) {
+      console.error("No timeslots found", error);
       res.status(404).send("No timeslot found");
       return;
     }
 
-    const firstTimeslot = timeslotList[0];
-
-    res.redirect(`/dj/${firstTimeslot.id}`);
+    res.redirect(`/dj/${timeslot.id}`);
   });
 
-  app.get("/dj/:timeslotId", (req, res) => {
+  app.get("/dj/:timeslotId", async (req, res) => {
     const { timeslotId } = req.params;
 
-    const idNum = parseInt(timeslotId);
+    let timeslots;
+    let timeslot;
+    try {
+      timeslots = await Timeslot.find();
+      timeslot = await Timeslot.findById(timeslotId);
+      if (timeslots === null || timeslots === undefined) {
+        throw new Error("Timeslots query returned null");
+      }
 
-    if (isNaN(idNum)) {
-      res.status(400).send("Playlist ID must be a number");
+      if (timeslot === undefined || timeslot === null) {
+        throw new Error("Timeslot query returned null");
+      }
+    } catch (error) {
+      console.error("Failed getting timeslot(s)", timeslotId, error);
+      res.status(404).send("Timeslot not found");
       return;
     }
 
-    const timeslot = timeslotList.find(
-      (slot) => slot.id === parseInt(timeslotId)
-    );
+    let playlist;
+    try {
+      playlist = await Playlist.findOne({ timeslot: timeslotId });
+      if (!playlist) {
+        throw new Error("Playlist not found");
+      }
+    } catch (error) {
+      console.error("Failed to fetch playlist", timeslotId);
+      res.status(500).send("Failed to fetch playlist");
+      return;
+    }
 
-    if (!timeslot) {
-      res.status(404).send("Playlist not found");
+    let playlistSongs;
+    try {
+      playlistSongs = await Song.find({
+        _id: { $in: playlist.songs.map((song) => song.song) },
+      });
+      if (playlistSongs === null || playlistSongs === undefined) {
+        throw new Error("Playlist songs query returned null");
+      }
+    } catch (error) {
+      console.error("Failed to fetch playlist songs", playlist.songs, error);
+      res.status(500).send("Failed to fetch playlist songs");
+      return;
+    }
+    playlistSongs.sort((a, b) => {
+      return (
+        playlist.songs.findIndex((song) => song.id === a.id) -
+        playlist.songs.findIndex((song) => song.id === b.id)
+      );
+    });
+
+    let allSongs;
+    try {
+      allSongs = await Song.find();
+      if (allSongs === undefined || allSongs === null) {
+        throw new Error("Songs query returned null");
+      }
+    } catch (error) {
+      console.error("Failed to fetch songs", error);
+      res.status(500).send("Failed to fetch songs");
       return;
     }
 
     res.render(join(__dirname, "../views/DJ/main.ejs"), {
-      timeslots: timeslotList.map((slot) => ({
+      timeslots: timeslots.map((slot) => ({
         ...slot,
-        selected: slot.id === idNum,
-        label: `${slot.date} ${slot.start_time} - ${slot.end_time}`,
+        id: slot.id.toString(),
+        selected: slot.id === timeslotId,
+        label: `${slot.tdate} ${slot.start} - ${slot.end}`,
       })),
       motd: "MOTD In Express!",
-      playlist: timeslot.playlist,
+      playlist: playlist,
+      playlistSongs,
+      allSongs,
     });
   });
 
-  app.put("/dj/api/playlist/:timeslotId/:id", (req, res) => {
+  app.post("/dj/api/playlist/:timeslotId", async (req, res) => {
+    const { timeslotId } = req.params;
+    const { songs: songIds } = req.body;
+
+    if (!Array.isArray(songIds)) {
+      console.error("Songs must be an array", songIds);
+      res.status(400).json({ error: "Songs must be an array" });
+      return;
+    }
+
+    let playlist;
+    try {
+      playlist = await Playlist.findOne({ timeslot: timeslotId });
+      if (playlist === null || playlist === undefined) {
+        throw new Error("Playlist query returned null");
+      }
+    } catch (error) {
+      console.error("Failed to find playlist", timeslotId, error);
+      res.status(404).json({ error: "Playlist not found" });
+      return;
+    }
+
+    let allSongs;
+    try {
+      allSongs = await Song.find({ _id: { $in: songIds } });
+      if (allSongs === null || allSongs === undefined) {
+        throw new Error("Songs query returned null");
+      }
+    } catch (error) {
+      console.error("Failed to find songs", songIds, error);
+      res.status(404).json({ error: "Songs not found" });
+      return;
+    }
+
+    playlist.songs = playlist.songs.concat(
+      allSongs.map((song) => {
+        return {
+          song: song.id,
+          producer_created: false,
+          dj_played: false,
+        };
+      })
+    );
+
+    let updatedPlaylist;
+    try {
+      updatedPlaylist = await Playlist.findByIdAndUpdate(playlist.id, {
+        songs: playlist.songs,
+      });
+
+      if (updatedPlaylist === null || updatedPlaylist === undefined) {
+        throw new Error("Playlist update returned null");
+      }
+    } catch (error) {
+      console.error("Failed to update playlist", playlist.id, error);
+      res.status(500).json({ error: "Failed to update playlist" });
+      return;
+    }
+
+    res.json(updatedPlaylist);
+  });
+
+  app.put("/dj/api/playlist/:timeslotId/:id", async (req, res) => {
     const { timeslotId, id } = req.params;
     const { title } = req.body;
 
     if (!title) {
+      console.error("Title is required", title);
       res.status(400).json({ error: "Title is required" });
       return;
     }
 
-    const idNum = parseInt(id);
-
-    if (isNaN(idNum)) {
-      res.status(400).json({ error: "Invalid song ID" });
+    let playlist;
+    try {
+      playlist = await Playlist.findOne({ timeslot: timeslotId });
+      if (playlist === null || playlist === undefined) {
+        throw new Error("Playlist query returned null");
+      }
+    } catch (error) {
+      console.error("Failed to find playlist", timeslotId, error);
+      res.status(404).json({ error: "Playlist not found" });
       return;
     }
 
-    const timeslotIdNum = parseInt(timeslotId);
+    const song = playlist.songs.find((song) => song.song == id);
 
-    if (isNaN(timeslotIdNum)) {
-      res.status(400).json({ error: "Invalid timeslot ID" });
-      return;
-    }
-
-    const timeslot = timeslotList.find((slot) => slot.id === timeslotIdNum);
-
-    if (!timeslot) {
-      res.status(404).json({ error: "Timeslot not found" });
-      return;
-    }
-
-    const song = timeslot.playlist.find((song) => song.id === idNum);
-
-    song.title = title;
-    res.json(song);
-  });
-
-  app.delete("/dj/api/playlist/:timeslotId/:id", (req, res) => {
-    const { timeslotId, id } = req.params;
-
-    const idNum = parseInt(id);
-
-    if (isNaN(idNum)) {
-      res.status(400).json({ error: "Invalid song ID" });
-      return;
-    }
-
-    const timeslotIdNum = parseInt(timeslotId);
-
-    if (isNaN(timeslotIdNum)) {
-      res.status(400).json({ error: "Invalid timeslot ID" });
-      return;
-    }
-
-    const timeslot = timeslotList.find((slot) => slot.id === timeslotIdNum);
-
-    if (timeslot === -1) {
-      res.status(404).json({ error: "Timeslot not found" });
-      return;
-    }
-
-    const index = timeslot.playlist.findIndex((song) => song.id === idNum);
-
-    if (index === -1) {
+    if (!song) {
+      console.error("Song not found in playlist", id, playlist.songs);
       res.status(404).json({ error: "Song not found" });
       return;
     }
 
-    timeslot.playlist.splice(index, 1);
+    let updatedSong;
+    try {
+      updatedSong = await Song.findByIdAndUpdate(id, { title: title });
+      if (updatedSong === null || updatedSong === undefined) {
+        throw new Error("Song update returned null");
+      }
+    } catch (error) {
+      console.error("Failed to update song", id, title, error);
+      res.status(500).json({ error: "Failed to update song" });
+      return;
+    }
+
+    res.json(updatedSong);
+  });
+
+  app.delete("/dj/api/playlist/:timeslotId/:id", async (req, res) => {
+    const { timeslotId, id } = req.params;
+
+    let playlist;
+    try {
+      playlist = await Playlist.findOne({ timeslot: timeslotId });
+      if (playlist === null || playlist === undefined) {
+        throw new Error("Playlist query returned null");
+      }
+    } catch (error) {
+      console.error("Failed to find playlist", timeslotId, error);
+      res.status(404).json({ error: "Playlist not found" });
+      return;
+    }
+
+    if (!playlist.songs.some((song) => song.song == id)) {
+      console.error("Song not found in playlist", id, playlist.songs);
+      res.status(404).json({ error: "Song not found" });
+      return;
+    }
+
+    playlist.songs = playlist.songs.filter((song) => song.song != id);
+
+    try {
+      const updatedPlaylist = await Playlist.findByIdAndUpdate(playlist.id, {
+        songs: playlist.songs,
+      });
+
+      if (updatedPlaylist === null || updatedPlaylist === undefined) {
+        throw new Error("Playlist update returned null");
+      }
+    } catch (error) {
+      console.error("Failed to update playlist", playlist.id, error);
+      res.status(500).json({ error: "Failed to update playlist" });
+      return;
+    }
+
     res.json({ success: true });
   });
 
-  app.put("/dj/api/playlist_order/:timeslotId", (req, res) => {
+  app.put("/dj/api/playlist_order/:timeslotId", async (req, res) => {
     const { timeslotId } = req.params;
     const order = req.body;
-
-    const timeslotIdNum = parseInt(timeslotId);
-
-    if (isNaN(timeslotIdNum)) {
-      res.status(400).json({ error: "Invalid timeslot ID" });
-      return;
-    }
 
     if (!Array.isArray(order)) {
       res.status(400).json({ error: "Order must be an array" });
       return;
     }
 
-    const timeslot = timeslotList.find((slot) => slot.id === timeslotIdNum);
-
-    if (!timeslot) {
-      res.status(404).json({ error: "Timeslot not found" });
+    let playlist;
+    try {
+      playlist = await Playlist.findOne({ timeslot: timeslotId });
+      if (playlist === null || playlist === undefined) {
+        throw new Error("Playlist not found");
+      }
+    } catch (error) {
+      console.error("Failed to find playlist", timeslotId, error);
+      res.status(404).json({ error: "Playlist not found" });
       return;
     }
 
-    if (order.length !== timeslot.playlist.length) {
+    if (order.length !== playlist.songs.length) {
       res.status(400).json({
         error: "Order must have the same number of items as the playlist",
       });
       return;
     }
 
-    const currentPlaylist = timeslot.playlist;
+    const newPlaylistWithOrdering = order.map((id, index) => {
+      const song = playlist.songs.find((slot) => slot.song == id);
 
-    const newPlaylist = order.map((id, index) => {
-      const timeslot = currentPlaylist.find((slot) => slot.id === id);
-
-      if (!timeslot) {
-        res.status(400).json({ error: `Timeslot with ID ${id} not found` });
+      if (!song) {
+        res.status(400).json({ error: `Song with ID ${id} not found` });
         return;
       }
 
-      timeslot.position = index + 1;
+      song.position = index;
 
-      return timeslot;
+      return song;
     });
 
-    currentPlaylist.length = 0;
-    newPlaylist.forEach((slot) => currentPlaylist.push(slot));
+    newPlaylistWithOrdering.sort((a, b) => {
+      return a.position - b.position;
+    });
+
+    const newPlaylist = newPlaylistWithOrdering.map((song) => ({
+      ...song,
+      position: undefined,
+    }));
+
+    try {
+      const updatedPlaylist = await Playlist.findByIdAndUpdate(playlist.id, {
+        songs: newPlaylist,
+      });
+
+      if (updatedPlaylist === null || updatedPlaylist === undefined) {
+        throw new Error("Playlist update returned null");
+      }
+    } catch (error) {
+      console.error("Failed to update playlist", playlist.id, error);
+      res.status(500).json({ error: "Failed to update playlist" });
+      return;
+    }
 
     res.json({ success: true });
   });
