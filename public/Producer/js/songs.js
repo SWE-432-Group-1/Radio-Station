3ca8b564661123011a89e0cffd60b8d10e0153ba
Rohit <83600150+Rohit-beep-droid@users.noreply.github.com>
2024-04-26 05:29:12 -0400

    Added delete, fixed dropdown, and fixed add
    Added delete functionality, and add, delete, dropdown *should* work as intended

--- public/Producer/js/songs.js
+++ public/Producer/js/songs.js
@@ -1,13 +1,13 @@
 // get playlist for djs
 document.querySelector("#djs").addEventListener("change", async function () {
     let dj = this.value;
-    const response = await fetch(`/producer/playlist/${dj}`);
-    if (response.ok) {
+    const res = await fetch(`/producer/playlist/${dj}`);
+    if (res.ok) {
         location.reload();
     }
 });
 
-// add songs to playlists
+// add songs to a playlist
 async function addSongToPlaylist() {
     const playlistName = prompt('Playlist name:');
     const songName = prompt('Song name:');
@@ -15,8 +15,22 @@ async function addSongToPlaylist() {
     if(!playlistName || !songName || !index)
         alert('One or more data was not provided!');
     else {
-        const response = await fetch(`/producer/playlist/addsong/${playlistName}/${songName}/${index}`);
-        if(response.ok) {
+        const res = await fetch(`/producer/playlist/addsong/${playlistName}/${songName}/${index}`);
+        if(res.ok) {
+            location.reload();
+        }
+    }
+}
+
+// remove song from a playlist
+async function removeSongFromPlaylist() {
+    const playlistName = prompt('Playlist name:');
+    const songName = prompt('Song name:');
+    if(!playlistName || !songName)
+        alert('One or more data was not provided!');
+    else {
+        const res = await fetch(`/producer/playlist/removesong/${playlistName}/${songName}`);
+        if(res.ok) {
             location.reload();
         }
     }
--- scripts/producer-router.js
+++ scripts/producer-router.js
@@ -9,6 +9,7 @@ const handleAll = (app) => {
   handleDefault(app);
   handleDJPlaylist(app);
   handleAddSong(app);
+  handleRemoveSong(app);
 };
 
 // list of songs
@@ -23,6 +24,7 @@ let djs = null;
 // list of playlists
 let playlists = null;
 
+// days of the calendar
 const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "1", "2", "3", "4", "5", "6", "7",
   "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22",
   "23", "24", "25", "26", "27", "28", "29"];
@@ -30,98 +32,16 @@ const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", "1", "2", "3", "4
 // gets the list of podcasts
 let podcasts = null;
 
-// adds song to playlist by order
-const handleAddSong = (app) => {
-  app.get("/producer/playlist/addsong/:playlistname/:songname/:index", async (req, res) => {
-    const playlistname = req.params.playlistname;
-    const songname = req.params.songname;
-    const index = req.params.index;
-    const findPlaylist = await Playlist.find({ name: playlistname });
-    const songlist = await Song.find({ title: songname });
-    const alreadyInPlaylist = [];
-    for (let s of findPlaylist[0].songs) {
-      if (s._id.toString() == songlist[0]._id.toString())
-        alreadyInPlaylist.push(s);
-    }
-    if (findPlaylist.length == 0 || songlist.length == 0
-      || alreadyInPlaylist.length > 0) {
-      console.log('Invalid name or playlist or already in the playlist!');
-      res.render(join(__dirname, "../views/Producer/pages/index"), {
-        djs: djs,
-        playlists: playlists,
-        days: days,
-        podcasts: podcasts,
-        songs: songs,
-        userHandler: "user();",
-      });
-    } else {
-      const addSong = {
-        _id: songlist[0]._id.toString(),
-        producer_created: true,
-        dj_played: false
-      };
-      const playlistID = findPlaylist[0]._id.toString();
-      Playlist.findOneAndUpdate({ _id: playlistID }, { $push: { songs: { $each: [addSong], $position: (parseInt(index) - 1) } } }, { new: true })
-        .then(updated => {
-          console.log(updated);
-          res.render(join(__dirname, "../views/Producer/pages/index"), {
-            djs: djs,
-            playlists: playlists,
-            days: days,
-            podcasts: podcasts,
-            songs: songs,
-            userHandler: "user();",
-          });
-        })
-        .catch(err => {
-          console.log('Failed:', err);
-        })
-    }
-  });
-}
-
-// displays dj playlist
-const handleDJPlaylist = (app) => {
-  app.get("/producer/playlist/:djname", async (req, res) => {
-    songs = [];
-    djSelected = true;
-    const djName = req.params.djname;
-    const foundDJ = await Dj.find({ name: djName });
-    const foundPlaylist = await Playlist.find();
-    const foundSongs = await Song.find();
-    const playlistbyDJID = foundPlaylist.find(p => p.dj == foundDJ[0]._id.toString());
-    if (playlistbyDJID === undefined) {
-      res.render(join(__dirname, "../views/Producer/pages/index"), {
-        djs: djs,
-        playlists: playlists,
-        days: days,
-        podcasts: podcasts,
-        songs: songs,
-        userHandler: "user();",
-      });
-    } else {
-      for (let s of foundSongs) {
-        for (let p of playlistbyDJID.songs) {
-          if (s._id.toString() == p._id.toString()) {
-            songs.push(s);
-          }
-        }
-      }
-      res.render(join(__dirname, "../views/Producer/pages/index"), {
-        djs: djs,
-        playlists: playlists,
-        days: days,
-        podcasts: podcasts,
-        songs: songs,
-        userHandler: "user();",
-      });
-    }
-  });
-}
+// dropdown dj name
+let djName = null;
 
 // default
 const handleDefault = (app) => {
   app.get("/producer", async (req, res) => {
+
+    // default dropdown
+    if (!djSelected)
+      djName = "Select DJ";
 
     // gets the list of podcasts
     podcasts = await Podcast.find();
@@ -140,6 +60,7 @@ const handleDefault = (app) => {
     playlists = await Playlist.find();
 
     res.render(join(__dirname, "../views/Producer/pages/index"), {
+      djName: djName,
       djs: djs,
       playlists: playlists,
       days: days,
@@ -147,6 +68,146 @@ const handleDefault = (app) => {
       songs: songs,
       userHandler: "user();",
     });
+  });
+}
+
+// adds song to playlist by order
+const handleAddSong = (app) => {
+  app.get("/producer/playlist/addsong/:playlistname/:songname/:index", async (req, res) => {
+    const playlistname = req.params.playlistname;
+    const songname = req.params.songname;
+    const index = req.params.index;
+    const findPlaylist = await Playlist.find({name:playlistname});
+    const songlist = await Song.find({title: songname});
+    const alreadyInPlaylist = [];
+    for (let s of findPlaylist[0].songs) {
+      if (s._id.toString() == songlist[0]._id.toString())
+        alreadyInPlaylist.push(s);
+    }
+    if (findPlaylist.length == 0 || songlist.length == 0
+      || alreadyInPlaylist.length > 0) {
+      console.log('Invalid name or playlist or already in the playlist!');
+      res.redirect('/producer');
+    } else {
+      const addSong = {
+        _id: songlist[0]._id.toString(),
+        producer_created: true,
+        dj_played: false
+      };
+      const playlistID = findPlaylist[0]._id.toString();
+      Playlist.findOneAndUpdate({_id: playlistID}, 
+        {$push: {songs: {$each: [addSong], $position: (parseInt(index))}}}, 
+        {new: true})
+        .then(async newPlaylist => {
+          console.log('Update success!');
+          songs = [];
+          const foundSongs = await Song.find();
+          for (let s of foundSongs) {
+            for (let i = 0; i < newPlaylist.songs.length; i++) {
+              if (s._id.toString() == newPlaylist.songs[i]._id.toString()) {
+                songs.splice(i, 0, s);
+                i++;
+              }
+            }
+          }
+          res.render(join(__dirname, "../views/Producer/pages/index"), {
+            djName: djName,
+            djs: djs,
+            playlists: playlists,
+            days: days,
+            podcasts: podcasts,
+            songs: songs,
+            userHandler: "user();",
+          });
+        })
+        .catch(err => {
+          console.log(`Failed! ${err}`);
+        })
+    }
+  });
+}
+
+// removes song from a playlist by songname
+const handleRemoveSong = (app) => {
+  app.get("/producer/playlist/removesong/:playlistname/:songname", async (req, res) => {
+    const playlistname = req.params.playlistname;
+    const songname = req.params.songname;
+    const findPlaylist = await Playlist.find({name: playlistname});
+    const songlist = await Song.find({title: songname});
+    const alreadyInPlaylist = [];
+    for(let s of findPlaylist[0].songs) {
+      if(s._id.toString() == songlist[0]._id.toString()) {
+        alreadyInPlaylist.push(s);
+      }
+    }
+    if(findPlaylist.length == 0 || songlist.length == 0
+      || alreadyInPlaylist.length == 0) {
+      console.log('Invalid name or playlist or already in the playlist!');
+      res.redirect('/producer');
+    } else {
+      const playlistID = findPlaylist[0]._id.toString(); 
+      const songId = songlist[0]._id.toString();
+      Playlist.findOneAndUpdate({_id: playlistID}, {$pull: {songs: {_id: songId}}}, {new: true})
+        .then(async newPlaylist => {
+          console.log('Update success!');
+          songs = [];
+          const foundSongs = await Song.find();
+          for (let s of foundSongs) {
+            for (let i = 0; i < newPlaylist.songs.length; i++) {
+              if (s._id.toString() == newPlaylist.songs[i]._id.toString()) {
+                songs.splice(i, 0, s);
+                i++;
+              }
+            }
+          }
+          res.render(join(__dirname, "../views/Producer/pages/index"), {
+            djName: djName,
+            djs: djs,
+            playlists: playlists,
+            days: days,
+            podcasts: podcasts,
+            songs: songs,
+            userHandler: "user();",
+          });
+        })
+        .catch(err => {
+          console.log(`Failed! ${err}`);
+        })
+    }
+  });
+}
+
+// displays dj playlist
+const handleDJPlaylist = (app) => {
+  app.get("/producer/playlist/:djname", async (req, res) => {
+    songs = [];
+    djSelected = true;
+    djName = req.params.djname;
+    const foundDJ = await Dj.find({ name: djName });
+    const foundPlaylist = await Playlist.find();
+    const foundSongs = await Song.find();
+    const playlistbyDjId = foundPlaylist.find(p => p.dj == foundDJ[0]._id.toString());
+    if (playlistbyDjId === undefined) {
+      res.redirect('/producer');
+    } else {
+      for (let s of foundSongs) {
+        for (let i = 0; i < playlistbyDjId.songs.length; i++) {
+          if (s._id.toString() == playlistbyDjId.songs[i]._id.toString()) {
+            songs.splice(i, 0, s);
+            i++;
+          }
+        }
+      }
+      res.render(join(__dirname, "../views/Producer/pages/index"), {
+        djName: djName,
+        djs: djs,
+        playlists: playlists,
+        days: days,
+        podcasts: podcasts,
+        songs: songs,
+        userHandler: "user();",
+      });
+    }
   });
 }
 
